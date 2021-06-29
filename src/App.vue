a5ff1002794db68e016e3671a8549d28a98c4d74
Konrad <konrad.gmur@gmail.com>
2021-06-29 20:51:16 +0200

    Creation and addition of navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
+  <div>
+    <div class="app flex">
+      <div class="app-content flex flex-column">
+        <Navigation />
+      </div>
+    </div>
+  </div>
   <router-view />
 </template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
