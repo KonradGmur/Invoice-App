f136ffaca89d95cbc957538fe3f3732f0e21f709
Konrad <konrad.gmur@gmail.com>
2021-06-30 19:15:37 +0200

    Creating a form part 1

---
+++ src/components/InvoiceModal.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    @click="checkClick"
+    ref="invoiceWrap"
+    class="invoice-wrap flex flex-column"
+  >
+    <form @submit.prevent="submitForm" class="invoice-content">
+      <h1>New Invoice</h1>
+
+      <!--Bill From -->
+      <div class="bill-from flex flex-column">
+        <h4>Bill From</h4>
+        <div class="input flex flex-column">
+          <label for="billerStreetAddress">Street Address</label>
+          <input
+            required
+            type="text"
+            id="billerStreetAddress"
+            v-model="billerStreetAddress"
+          />
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "invoiceModal",
+  data() {
+    return {
+      billerStreetAddress: null,
+      billerCity: null,
+      billerZipCode: null,
+      billerCountry: null,
+      clientName: null,
+      clientEmail: null,
+      clientStreetAddress: null,
+      clientCity: null,
+      clientZipCode: null,
+      clientCountry: null,
+      invoiceDateUnix: null,
+      invoiceDate: null,
+      paymentTerms: null,
+      paymentDueDateUnix: null,
+      paymentDueDate: null,
+      productDescription: null,
+      invoicePending: null,
+      invoiceDraft: null,
+      invoiceItemList: [],
+      invoiceTotal: 0,
+    };
+  },
+};
+</script>
+
+<style lang="scss"></style>
