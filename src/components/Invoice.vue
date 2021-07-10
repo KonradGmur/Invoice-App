019778e401ac83f3ebe6c844c7b218fd667097ac
Konrad <konrad.gmur@gmail.com>
2021-07-10 20:40:06 +0200

    Displaying invoices on the main page Part 2

--- src/components/Invoice.vue
+++ src/components/Invoice.vue
@@ -1,9 +1,12 @@
 <template>
   <router-link
     class="invoice flex"
-    :to="{ name: 'Invoice', params: { invoiceId: invoice.invoiceId } }">
+    :to="{ name: 'Invoice', params: { invoiceId: invoice.invoiceId } }"
+  >
     <div class="left flex">
-      
+      <span class="tracking-number">{{ invoice.invoiceId }}</span>
+      <span class="due-date">{{ invoice.paymentDueDate }}</span>
+      <span class="person">{{ invoice.clientName }}</span>
     </div>
   </router-link>
 </template>
