6b6bc105dd92d3edd657f6819326956ff576e3e9
Konrad <konrad.gmur@gmail.com>
2021-07-14 19:41:40 +0200

    Adding logic to the edit button part 2

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,54 +1,61 @@
 <template>
-  <div class="modal flex">
-    <div class="modal-content">
-      <p>Are you sure you want to exit? Your change will not be saved?</p>
-      <div class="actions flex">
-        <button @click="closeModal" class="purple">Return</button>
-        <button @click="closeInvoice" class="red">Close</button>
-      </div>
-    </div>
-  </div>
+	<div class="modal flex">
+		<div class="modal-content">
+			<p>Are you sure you want to exit? Your change will not be saved?</p>
+			<div class="actions flex">
+				<button @click="closeModal" class="purple">Return</button>
+				<button @click="closeInvoice" class="red">Close</button>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
-import { mapMutations } from "vuex";
+import { mapMutations, mapState } from 'vuex';
 export default {
-  name: "modal",
-  methods: {
-    ...mapMutations(["TOGGLE_MODAL", "TOOGLE_INVOICE"]),
+	name: 'modal',
+	methods: {
+		...mapMutations(['TOGGLE_MODAL', 'TOOGLE_INVOICE', 'TOGGLE_EDIT_INVOICE']),
 
-    closeModal() {
-      this.TOGGLE_MODAL();
-    },
+		closeModal() {
+			this.TOGGLE_MODAL();
+		},
 
-    closeInvoice() {
-      this.TOOGLE_INVOICE();
-    }, 
-  },
+		closeInvoice() {
+			this.TOOGLE_INVOICE();
+			this.TOGGLE_MODAL();
+			if (this.editInvoice) {
+				TOGGLE_EDIT_INVOICE();
+			}
+		},
+	}, 
+	computed: {
+		...mapState(['editInvoice']),
+	},
 };
 </script>
 <style lang="scss" scoped>
 .modal {
-  z-index: 100;
-  position: fixed;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100%;
-  .modal-content {
-    border-radius: 20px;
-    padding: 48px 32px;
-    max-width: 450px;
-    background-color: #252945;
-    color: #fff;
-    p {
-      text-align: center;
-    }
-    .actions {
-      margin-top: 24px;
-      button {
-        flex: 1;
-      }
-    }
-  }
+	z-index: 100;
+	position: fixed;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	width: 100%;
+	.modal-content {
+		border-radius: 20px;
+		padding: 48px 32px;
+		max-width: 450px;
+		background-color: #252945;
+		color: #fff;
+		p {
+			text-align: center;
+		}
+		.actions {
+			margin-top: 24px;
+			button {
+				flex: 1;
+			}
+		}
+	}
 }
 </style>
