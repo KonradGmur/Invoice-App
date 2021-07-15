8d163dd79ae6247b319ff7a929a44ee9ceec199e
Konrad <konrad.gmur@gmail.com>
2021-07-15 19:42:20 +0200

    Removing an invoice with Delete and return to home

--- src/views/invoiceView.vue
+++ src/views/invoiceView.vue
@@ -122,6 +122,8 @@ export default {
 			'TOGGLE_INVOICE',
 		]),
 
+		...mapActions(['DELETE_INVOICE']),
+
 		getCurrentInvoice() {
 			this.SET_CURRENT_INVOICE(this.$route.params.invoiceId);
 			this.currentInvoice = this.currentInvoiceArray[0];
@@ -129,6 +131,10 @@ export default {
 		toggleEditInvoice() {
 			this.TOGGLE_EDIT_INVOICE();
 			this.TOGGLE_INVOICE();
+		},
+		async deleteInvoice(docId) {
+			await this.DELETE_INVOICE(docId);
+			this.$router.push({ name: 'Home' });
 		},
 	},
 	computed: {
